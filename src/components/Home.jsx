122c782343b619c7f77aa951ba3d2e9a7b280864
Cmendence <chris.mendence@gmail.com>
2024-04-30 13:03:40 -0400

    adding more

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,9 @@ import Logo from '../assets/logo.svg'
 export default function Home () {
 
    return (
-         <div className='bg-slate-300 h-screen w-screen'>
+         <div className='bg-slate-300 h-screen w-screen flex justify-center'>
             <div className='text-center'>
-            <img src={Logo}/>
+            <img src={Logo} className='h-1/3'/>
             <h1>HOME PAGE</h1>
             </div>
          </div>
