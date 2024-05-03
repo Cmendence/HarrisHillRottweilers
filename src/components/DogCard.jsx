2d820c758225219e85e0c627a3e467a2aa0a2a9d
Cmendence <chris.mendence@gmail.com>
2024-05-03 10:03:30 -0400

    building dog card

--- src/components/DogCard.jsx
+++ src/components/DogCard.jsx
@@ -1,8 +1,15 @@
 import React from "react"
 
-export default function DogCard() {
-   
+export default function DogCard({dog}) {
+
    return(
-      
+
+   <div>
+        <div className='lg:w-48 w-24 rounded-lg text-center lg:m-4 m-2'>
+             <img src={dog.image} className='rounded-lg shadow-lg shadow-gray-600'/>    
+             <h2 className='font-semibold lg:text-2xl text-lg m-2 text-gray-800'>{dog.name}</h2>
+             <h2 className='font-semibold lg:text-xl text-sm m-2 text-gray-800'>Age: {dog.age}</h2>
+         </div>
+   </div>
    )
 }
--- src/components/Males.jsx
+++ src/components/Males.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import {dogs} from '../assets/dogs'
+import DogCard from './DogCard'
 
 
 export default function Males() {
-   
+
+
    return(
-      <div>
-         
-         <h1 className='text-4xl text-red-500 underline font-extrabold text-center'> THIS IS THE MALES PAGE</h1>
+      <div className='flex justify-center lg:my-4 flex-wrap lg:mx-8'>
+         {dogs.map( dog => (
+         // <div className='w-1/5 rounded-lg text-center m-4' key={dog.name}>
+         //     <img src={dog.image} className='rounded-lg shadow-lg shadow-gray-600'/>    
+         //     <h2 className='font-semibold text-2xl m-2 text-gray-800'>{dog.name}</h2>
+         //     <h2 className='font-semibold text-xl m-2 text-gray-800'>Age: {dog.age}</h2>
+         // </div>
+         <DogCard 
+            key={dog.name}
+            dog={dog}
+         />
+         ))}
       </div>
    )
 }
