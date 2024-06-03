e0121e786cf86ba9e06ab4ce47e61d3e042edb17
Cmendence <chris.mendence@gmail.com>
2024-06-03 12:42:19 -0400

    added admin email

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
 
       <div>
          <h1 className='text-xl font-semibold'> Admin Dashboard</h1>
-         <h1 className='text-4xl font-semibold'> PLEASE LET THIS WORK</h1>
+         <h1 className='text-4xl font-semibold'> Yay! It&apos;s working!</h1>
       </div>
 
    )
