e5d7b37e8f6bfd46d3e5fa47eade4be9f2c168f0
Cmendence <chris.mendence@gmail.com>
2024-06-25 11:26:35 -0400

    optimized DashDogCard to show name above images, cert names, code cleanup

--- src/components/DashDogCard.jsx
+++ src/components/DashDogCard.jsx
@@ -16,6 +16,8 @@ export default function DashDogCard({
       key={dog.id}
       className="bg-gray-800 p-4 rounded-lg shadow-lg shadow-gray-700"
     >
+      <h3 className="text-lg font-bold text-gray-100 mb-1">{dog.name}</h3>
+
       <div className="grid grid-cols-2 gap-3">
         {dog.images.map((image, index) => (
           <img
@@ -26,19 +28,33 @@ export default function DashDogCard({
           />
         ))}
       </div>
-      <h3 className="text-lg font-bold text-gray-100">{dog.name}</h3>
-      <p className="text-gray-300">Gender: {findGender}</p>
-      <p className="text-gray-300">Age: {calculateAge(dog.birthdate)}</p>
-      <p className="text-gray-300">Mother: {dog.mother}</p>
-      <p className="text-gray-300">Father: {dog.father}</p>
-      <p className="text-gray-300">Birthdate: {dog.birthdate}</p>
-      <p className="text-gray-300">
-        Tags:{" "}
-        {dog.tags.map((tag) => (
-          <p key={tag}>-{tag}</p>
-        ))}
-      </p>
-
+      <div>
+        <div>
+          <div className="text-gray-300">
+            <p>Gender: {findGender}</p>
+            <p>Age: {calculateAge(dog.birthdate)}</p>
+            <p>Mother: {dog.mother}</p>
+            <p>Father: {dog.father}</p>
+            <p>Birthdate: {dog.birthdate}</p>
+            <p className="text-yellow-500">
+              Tags:{" "}
+              {dog.tags.map((tag) => (
+                <p key={tag} className="text-gray-300">
+                  -{tag}
+                </p>
+              ))}
+            </p>
+          </div>
+        </div>
+        <p className="text-yellow-500">Certs:</p>
+        <div>
+          {dog.certs.map((cert, index) => (
+            <p key={index} className="text-gray-300">
+              {cert.name}
+            </p>
+          ))}
+        </div>
+      </div>
       <div className="mt-4 flex justify-between items-start">
         <button
           onClick={() => handleEditDog(dog.id)}
@@ -55,13 +71,13 @@ export default function DashDogCard({
                   onClick={handleDeleteDog}
                   className="bg-rose-700 text-white p-2 rounded mr-2"
                 >
-                  Yes
+                  Delete
                 </button>
                 <button
                   onClick={cancelDelete}
                   className="bg-gray-500 text-white p-2 rounded"
                 >
-                  No
+                  Cancel
                 </button>
               </div>
             </div>
